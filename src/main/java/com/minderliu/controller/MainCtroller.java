abb9c3b1b0a9d5af10d70610488cd6bf23947993
liuqi <pjlqcumt@126.com>
2017-05-11 11:11:45 +0800

    mybatis集成OK

--- src/main/java/com/minderliu/controller/MainCtroller.java
+++ src/main/java/com/minderliu/controller/MainCtroller.java
@@ -3,10 +3,7 @@ package com.minderliu.controller;
 import com.minderliu.entity.Fund;
 import com.minderliu.entity.FundKey;
 import com.minderliu.entity.User;
-import com.minderliu.service.IFundService;
 import com.minderliu.service.impl.FundServiceImpl;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,9 +21,20 @@ public class MainCtroller {
     private FundServiceImpl fundService;
 
     @RequestMapping("/a")
-    public @ResponseBody Fund test(Model model) {
+    public @ResponseBody
+    Fund test() {
        Fund a= fundService.selectByPrimaryKey(new FundKey(3287,"519066"));
+
         return a;
+    }
+
+    @RequestMapping("/b")
+    public @ResponseBody
+    User test2() {
+       User u=new User();
+       u.setId(1);
+
+        return u;
     }
 
 }
