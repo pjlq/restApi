4e816cf7032c4353dbb8b7facb1b6a324193c0cb
liuqi <pjlqcumt@126.com>
2017-05-11 16:31:18 +0800

    swagger  集成  （api显示有问题 ）

--- src/main/java/com/minderliu/controller/MainCtroller.java
+++ src/main/java/com/minderliu/controller/MainCtroller.java
@@ -4,6 +4,8 @@ import com.minderliu.entity.Fund;
 import com.minderliu.entity.FundKey;
 import com.minderliu.entity.User;
 import com.minderliu.service.impl.FundServiceImpl;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,26 +17,27 @@ import javax.annotation.Resource;
  */
 @RestController
 @RequestMapping
+@Api(value = "admin")
 public class MainCtroller {
 
     @Resource
     private FundServiceImpl fundService;
 
     @RequestMapping("/a")
-    public @ResponseBody
-    Fund test() {
+    @ResponseBody
+    @ApiOperation(value = "ww", httpMethod = "GET", response = Fund.class, notes = "ww")
+    public  Fund test() {
        Fund a= fundService.selectByPrimaryKey(new FundKey(3287,"519066"));
-
         return a;
     }
 
     @RequestMapping("/b")
-    public @ResponseBody
-    User test2() {
+    @ResponseBody
+    @ApiOperation(value = "test", httpMethod = "GET", response = User.class, notes = "ww")
+    public User test2() {
        User u=new User();
        u.setId(1);
-
-        return u;
+       return u;
     }
 
 }
